a3415af230878a5900f2e80f0a78d1562d15748b
carmholm <ameliacarmenholmes@gmail.com>
2016-04-28 21:59:08 +0000

    Add a getAllPostsForUser(userId, options, callback) function

--- reddit.js
+++ reddit.js
@@ -4,23 +4,16 @@ var HASH_ROUNDS = 10;
 module.exports = function RedditAPI(conn) {
   return {
     createUser: function(user, callback) {
-      
-      // first we have to hash the password...
+
       bcrypt.hash(user.password, HASH_ROUNDS, function(err, hashedPassword) {
         if (err) {
           callback(err);
         }
         else {
           conn.query(
-            'INSERT INTO `users` (`username`,`password`, `createdAt`) VALUES (?, ?, ?)', 
-            [user.username, hashedPassword, null],
+            'INSERT INTO `users` (`username`,`password`, `createdAt`) VALUES (?, ?, ?)', [user.username, hashedPassword, null],
             function(err, result) {
               if (err) {
-                /*
-                There can be many reasons why a MySQL query could fail. While many of
-                them are unknown, there's a particular error about unique usernames
-                which we can be more explicit about!
-                */
                 if (err.code === 'ER_DUP_ENTRY') {
                   callback(new Error('A user with this username already exists'));
                 }
@@ -29,11 +22,6 @@ module.exports = function RedditAPI(conn) {
                 }
               }
               else {
-                /*
-                Here we are INSERTing data, so the only useful thing we get back
-                is the ID of the newly inserted row. Let's use it to find the user
-                and return it
-                */
                 conn.query(
                   'SELECT `id`, `username`, `createdAt`, `updatedAt` FROM `users` WHERE `id` = ?', [result.insertId],
                   function(err, result) {
@@ -41,15 +29,7 @@ module.exports = function RedditAPI(conn) {
                       callback(err);
                     }
                     else {
-                      /*
-                      Finally! Here's what we did so far:
-                      1. Hash the user's password
-                      2. Insert the user in the DB
-                      3a. If the insert fails, report the error to the caller
-                      3b. If the insert succeeds, re-fetch the user from the DB
-                      4. If the re-fetch succeeds, return the object to the caller
-                      */
-                        callback(null, result[0]);
+                      callback(null, result[0]);
                     }
                   }
                 );
@@ -67,10 +47,6 @@ module.exports = function RedditAPI(conn) {
             callback(err);
           }
           else {
-            /*
-            Post inserted successfully. Let's use the result.insertId to retrieve
-            the post and send it to the caller!
-            */
             conn.query(
               'SELECT `id`,`title`,`url`,`userId`, `createdAt`, `updatedAt` FROM `posts` WHERE `id` = ?', [result.insertId],
               function(err, result) {
@@ -87,15 +63,14 @@ module.exports = function RedditAPI(conn) {
       );
     },
     getAllPosts: function(options, callback) {
-      // In case we are called without an options parameter, shift all the parameters manually
       if (!callback) {
         callback = options;
         options = {};
       }
       var limit = options.numPerPage || 25; // if options.numPerPage is "falsy" then use 25
       var offset = (options.page || 0) * limit;
-      
-conn.query(`
+
+      conn.query(`
         SELECT p.id AS postId, p.title, p.url, p.userId, p.createdAt, p.updatedAt, u.id AS userId, u.username, u.createdAt AS userCreatedAt, u.updatedAt AS userUpdatedAt
         FROM posts p 
         JOIN users u 
@@ -104,32 +79,60 @@ conn.query(`
         LIMIT ? OFFSET ?
         `, [limit, offset],
         function(err, results) {
-          
+
           if (err) {
             callback(err);
           }
           else {
-            var newResults = results.map(function(obj){
-                  var newObj = {};
-                  var userObj = {};
-                  newObj["id"] = obj.id;
-                  newObj["title"] = obj.title;
-                  newObj["url"] = obj.url;
-                  newObj["createdAt"] = obj.createdAt;
-                  newObj["updatedAt"] = obj.updatedAt;
-                  newObj["userId"] = obj.userId;
-                  newObj["user"] = userObj;
-                  userObj["id"] = obj.userId;
-                  userObj["username"] = obj.username;
-                  userObj["createdAt"] = obj.userCreatedAt;
-                  userObj["updatedAt"] = obj.userUpdatedAt;
-                  return newObj;
-                });
-                callback(null, newResults);
+            var newResults = results.map(function(obj) {
+              var newObj = {};
+              var userObj = {};
+              newObj["id"] = obj.id;
+              newObj["title"] = obj.title;
+              newObj["url"] = obj.url;
+              newObj["createdAt"] = obj.createdAt;
+              newObj["updatedAt"] = obj.updatedAt;
+              newObj["userId"] = obj.userId;
+              newObj["user"] = userObj;
+              userObj["id"] = obj.userId;
+              userObj["username"] = obj.username;
+              userObj["createdAt"] = obj.userCreatedAt;
+              userObj["updatedAt"] = obj.userUpdatedAt;
+              return newObj;
+            });
+            callback(null, newResults);
+          }
+        }
+      );
+    },
+    getAllPostsForUser: function(userId, options, callback) {
+      if (!callback) {
+        callback = options;
+        options = {};
+      }
+      var limit = options.numPerPage || 25;
+      var offset = (options.page || 0) * limit;
+
+      conn.query(`
+        SELECT id AS postId, title, url, createdAt, updatedAt
+        FROM posts p
+        WHERE userId= ?
+        ORDER BY p.id DESC
+        LIMIT ? OFFSET ?
+        `, [userId, limit, offset],
+
+        function(err, results) {
+          if (err) {
+            callback(err);
+          }
+          else if (!results[0]) {
+            callback(new Error("This user does not have any posts"));
+          }
+          else {
+            callback(null, results);
           }
         }
       );
     }
   };
 };
-
